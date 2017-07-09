76f72cfda1c196d453d3a89a5b4bd07e4aed01ca
flowstone <yaoxue2014@hotmail.com>
2017-07-09 19:40:59 +0800

    数组中的字符串是否对称

---
+++ LastTimeExam/src/com/itheima/cn/Test18.java
@@ -0,0 +1,24 @@
+package com.itheima.cn;
+/**
+ * 二.(1)定义数字字符串数组{"010","3223","666","7890987","123123"} 
+(2)判断该数字字符串数组中的数字字符串是否是对称(第一个数字和最后一个数字相等，第二个数字和倒数第二个数字是相等的，依次类推)的，并逐个输出 
+如：010 是对称的，3223 是对称的，123123 不是对称的。
+ * @author Yao Xue
+ * @date Jul 9, 2017 6:47:26 PM
+ */
+public class Test18 {
+    public static void main(String[] args) {
+        String[] str = {"010","3223","666","7890987","123123"} ;
+        String[] strings = new String[str.length];
+        for (int i = 0; i < str.length; i++) {
+            strings[i]  = str[i];
+            StringBuilder stringBuilder = new StringBuilder(str[i]);
+            
+            if (strings[i].contentEquals(stringBuilder.reverse())) {
+                System.out.println(strings[i]+" 是对称的");
+            } else {
+               System.out.println(strings[i]+" 不是对称的");
+            }
+        }
+    }
+}
