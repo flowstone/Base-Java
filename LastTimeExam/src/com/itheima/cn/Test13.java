787ffbaa65bd0a3674784f7f22b82078277467cf
flowstone <yaoxue2014@hotmail.com>
2017-07-10 19:19:04 +0800

    截取出来的字符串长度，将长度和截取出的字符串在控制台打印输出-方法二

--- LastTimeExam/src/com/itheima/cn/Test13.java
+++ LastTimeExam/src/com/itheima/cn/Test13.java
@@ -8,10 +8,22 @@ package com.itheima.cn;
  */
 public class Test13 {
     public static void main(String[] args) {
+        //String str = "   ab12adfasdfsas3456465cd  ";
         String str = "   ab123cd  ";
-        String str1 = str.trim().substring(2, 5);
+        /*String str1 = str.trim().substring(2, 5);
         int len = str1.length();
         
-        System.out.println("长度："+len+"截取的字符："+str1);
+        System.out.println("长度："+len+"截取的字符："+str1);*/
+        
+        //方法二
+        String str1 = str.trim();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) >= '0' && str1.charAt(i) <= '9') {
+                sb.append(str1.charAt(i));
+            }
+        }
+        String s = sb.toString();
+        System.out.println("长度为"+s.length()+"截取出的字符串"+s);
     }
 }
