b8914191f803a6ba906082aa0e7badbcc68cf921
flowstone <yaoxue2014@hotmail.com>
2017-07-12 20:22:33 +0800

    javadoc-生成

---
+++ OtherPracticeTest/src/org/xueyao/day007/Demo1_ArrayTool.java
@@ -0,0 +1,26 @@
+package org.xueyao.day007;
+
+
+/**
+ * @author Yao Xue
+ * @date Jul 10, 2017 8:28:57 PM
+ */
+public class Demo1_ArrayTool {
+    public static void main(String[] args) {
+        int[] arr = {33,11,22,66,55,44};
+       /* ArrayTool arrayTool = new ArrayTool();
+        int max = arrayTool.getMax(arr);
+        System.out.println("max:"+max);
+        
+        System.out.println("----------");
+        arrayTool.print(arr);
+        
+        System.out.println();
+        System.out.println("-------");
+        System.out.println("·´×ªºó£º");
+        arrayTool.revArray(arr);
+        arrayTool.print(arr);*/
+        
+        ArrayTool.print(arr);
+    }
+}
