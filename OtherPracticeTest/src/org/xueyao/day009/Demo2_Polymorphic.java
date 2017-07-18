b9fe25c1add1cb200cead158d44468bf97624518
flowstone <xueyao.me@gmail.com>
2017-07-18 09:17:28 +0800

    多态中成员访问特点之成员变量和成员方法

--- OtherPracticeTest/src/org/xueyao/day009/Demo2_Polymorphic.java
+++ OtherPracticeTest/src/org/xueyao/day009/Demo2_Polymorphic.java
@@ -1,19 +1,31 @@
 package org.xueyao.day009;
 /**
+ * 成员变量
+ * 编译看左边(父类)，运行看左边(父类)
+ * 
+ * 成员方法
+ * 编译看左边(父类)，运行看右边(子类)。 动态绑定
  * @author Yao Xue
  * @date Jul 17, 2017 2:20:21 PM
  */
 public class Demo2_Polymorphic {
     public static void main(String[] args) {
         Father f = new Son();
-        System.out.println(f.num);
+        f.print();
+        //System.out.println(f.num);
     }
 }
 
 class Father {
     int num = 10;
+    public void print() {
+        System.out.println("father");
+    }
 }
 
 class Son extends Father {
     int num = 20;
+    public void print() {
+        System.out.println("son");
+    }
 }
