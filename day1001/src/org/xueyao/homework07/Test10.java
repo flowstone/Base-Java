abdd45e415c65459608ac59ccc8236f0b79ce78c
flowstone <xueyao.me@gmail.com>
2017-07-19 10:50:08 +0800

    对象之组合关系

--- day1001/src/org/xueyao/homework07/Test10.java
+++ day1001/src/org/xueyao/homework07/Test10.java
@@ -5,5 +5,63 @@ package org.xueyao.homework07;
  * @date Jul 19, 2017 9:13:49 AM
  */
 public class Test10 {
+    public static void main(String[] args) {
+        new Boy("李晨", 45, new Girl("范冰冰", 20)).walking();
+    }
+}
 
+
+class Girl  {
+    private String name;
+    private int age;
+    public Girl(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+}
+class Boy {
+    private String name;
+    private int age;
+    private Girl girlFriend;
+    public Boy(String name, int age, Girl girlFriend) {
+        this.name = name;
+        this.age = age;
+        this.girlFriend = girlFriend;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public Girl getGirlFriend() {
+        return girlFriend;
+    }
+    public void setGirlFriend(Girl girlFriend) {
+        this.girlFriend = girlFriend;
+    }
+    //45岁的大男孩李晨在和20岁的小女友范冰冰逛街
+    public void walking() {
+        System.out.println(age+"岁的大男孩"+name+"在和"+girlFriend.getAge()+"岁的小女友"+girlFriend.getName()+"逛街");
+    }
+    
 }
