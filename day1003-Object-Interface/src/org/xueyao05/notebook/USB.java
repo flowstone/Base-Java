8d5d8797bad0db8223eba2dbf82a1c6012344e2d
flowstone <xueyao.me@gmail.com>
2017-07-21 11:56:18 +0800

    笔记本电脑案例

---
+++ day1003-Object-Interface/src/org/xueyao05/notebook/USB.java
@@ -0,0 +1,10 @@
+package org.xueyao05.notebook;
+/**
+ * @description USB接口，包含开启功能、关闭功能
+ * @author Yao Xue
+ * @date Jul 21, 2017 11:37:42 AM
+ */
+public interface USB {
+    public void open();
+    public void close();
+}
