31b228b55b0068221c07c8b631d27e4b1cb42c4f
flowstone <xueyao.me@gmail.com>
2017-07-21 11:33:37 +0800

    多态中instanceof的使用

--- day1003-Object-Interface/src/org/xueyao04/polymiorphism/Demo3.java
+++ day1003-Object-Interface/src/org/xueyao04/polymiorphism/Demo3.java
@@ -30,8 +30,21 @@ public class Demo3 {
         //Actress a = (Actress)p;
         //a.beFamous();
         
-        Barber b = (Barber)p;
-        b.chasingGirls();
+        //Barber b = (Barber)p;
+        //b.chasingGirls();
+        
+        //如何调用子类私有的方法？
+        //需要根据传入的不同对象，进行不同对象的 强制类型转换
+        if (p instanceof Barber) {
+            Barber b = (Barber)p;
+            b.chasingGirls();
+        } else if (p instanceof Actress) {
+            Actress a = (Actress)p;
+            a.beFamous();
+        } else if (p instanceof Doctor) {
+            Doctor d = (Doctor)p;
+            d.temptationOfUniforms();
+        }
     }
     
     
