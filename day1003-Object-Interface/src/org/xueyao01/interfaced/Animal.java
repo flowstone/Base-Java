1808268eb0a86da39b9ec34fcd003ed59de8487d
flowstone <xueyao.me@gmail.com>
2017-07-21 11:57:06 +0800

    接口介绍

---
+++ day1003-Object-Interface/src/org/xueyao01/interfaced/Animal.java
@@ -0,0 +1,11 @@
+package org.xueyao01.interfaced;
+/**
+ * @author Yao Xue
+ * @date Jul 21, 2017 8:31:52 AM
+ */
+public abstract class Animal {
+    public abstract void shout();
+    
+    public abstract void eat();
+    
+}
---
+++ day1003-Object-Interface/src/org/xueyao01/interfaced/Test.java
@@ -0,0 +1,11 @@
+package org.xueyao01.interfaced;
+/**
+ * @author Yao Xue
+ * @date Jul 21, 2017 8:39:54 AM
+ */
+public class Test {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+    }
+}
