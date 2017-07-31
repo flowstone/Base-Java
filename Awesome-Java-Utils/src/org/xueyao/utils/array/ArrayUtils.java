22e731c66ec07013c14d9d6e31054fe3a7ed68f8
flowstone <xueyao.me@gmail.com>
2017-07-31 17:09:27 +0800

    数组工具类修改-2017-07-31

--- Awesome-Java-Utils/src/org/xueyao/utils/array/ArrayUtils.java
+++ Awesome-Java-Utils/src/org/xueyao/utils/array/ArrayUtils.java
@@ -1,35 +1,14 @@
-package org.xueyao.tools;
+package org.xueyao.utils.array;
 
-import java.io.Closeable;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 /** 
- * @description 常用工具类
+ * @description 数组工具类
  * @author Yao Xue
  * @date Jul 26, 2017 8:16:39 AM
  */
-public class Functions {
-    public static void main(String[] args) {
-        
-    }
-    /**
-     * @description: 关闭输入输入流对象
-     * @param ios 输入输出流对象 
-     * 
-     */
-    public static void closeTo(Closeable ... ios) {
-        for (Closeable io : ios) {
-            if (io != null) {
-                try {
-                    io.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+public class ArrayUtils {
     
     /**
      * @description 遍历任意类型的ArrayList
