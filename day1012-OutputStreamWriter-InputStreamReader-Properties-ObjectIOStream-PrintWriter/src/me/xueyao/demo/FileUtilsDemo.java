b19277c939336d34ef8ec7e1a28ba5aaa3d286a4
flowstone <xueyao.me@gmail.com>
2017-08-03 22:25:44 +0800

    第三方库-commons.io-2.4.jar

---
+++ day1012-OutputStreamWriter-InputStreamReader-Properties-ObjectIOStream-PrintWriter/src/me/xueyao/demo/FileUtilsDemo.java
@@ -0,0 +1,13 @@
+package me.xueyao.demo;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ * @author Yao Xue
+ * @date Aug 3, 2017 2:52:15 PM
+ */
+public class FileUtilsDemo {
+    public static void main(String[] args) {
+        //FileUtils.copyFile(srcFile, destFile);
+    }
+}
