58d89b8146f02a43112a1a8cd802415f3745786e
flowstone <xueyao.me@gmail.com>
2017-08-17 14:57:41 +0800

    sql-连接池-DBuntils

--- day4023-JDBC-ConnectionPools-DButils/db.sql
+++ day4023-JDBC-ConnectionPools-DButils/db.sql
@@ -5,3 +5,28 @@ CREATE TABLE IF NOT EXISTS user(
 	password VARCHAR(20) NOT NULL COMMENT '密码',
 	PRIMARY KEY (id)
 )CHARACTER SET utf8 COMMENT '用户表';
+
+创建表：
+create table product(
+	pid int primary key,
+	pname varchar(20),
+	price double,
+	category_id varchar(32)
+);
+INSERT INTO product(pid,pname,price,category_id) VALUES(1,'联想',5000,'c001');
+INSERT INTO product(pid,pname,price,category_id) VALUES(2,'海尔',3000,'c001');
+INSERT INTO product(pid,pname,price,category_id) VALUES(3,'雷神',5000,'c001');
+
+INSERT INTO product(pid,pname,price,category_id) VALUES(4,'JACK JONES',800,'c002');
+INSERT INTO product(pid,pname,price,category_id) VALUES(5,'真维斯',200,'c002');
+INSERT INTO product(pid,pname,price,category_id) VALUES(6,'花花公子',440,'c002');
+INSERT INTO product(pid,pname,price,category_id) VALUES(7,'劲霸',2000,'c002');
+
+INSERT INTO product(pid,pname,price,category_id) VALUES(8,'香奈儿',800,'c003');
+INSERT INTO product(pid,pname,price,category_id) VALUES(9,'相宜本草',200,'c003');
+INSERT INTO product(pid,pname,price,category_id) VALUES(10,'面霸',5,'c003');
+
+INSERT INTO product(pid,pname,price,category_id) VALUES(11,'好想你枣',56,'c004');
+INSERT INTO product(pid,pname,price,category_id) VALUES(12,'香飘飘奶茶',1,'c005');
+
+INSERT INTO product(pid,pname,price,category_id) VALUES(13,'果9',1,NULL);
