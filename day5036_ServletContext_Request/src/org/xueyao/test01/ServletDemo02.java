591242498f928c53414cc94924b95e9558e96d41
flowstone <xueyao.me@gmail.com>
2017-09-03 21:27:27 +0800

    利用request获取请求头

--- day5036_ServletContext_Request/src/org/xueyao/test01/ServletDemo02.java
+++ day5036_ServletContext_Request/src/org/xueyao/test01/ServletDemo02.java
@@ -1,37 +1,32 @@
 package org.xueyao.test01;
 
 import java.io.IOException;
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ServletDemo02
- */
 public class ServletDemo02 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletDemo02() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		//利用request获取请求头
+		
+		//获取到所有请求头名称
+		Enumeration<String> names = request.getHeaderNames();
+		while (names.hasMoreElements()) {
+			//获取到所有的请求头
+			String name = names.nextElement();
+			//根据name获取对应的请求头的值
+			String value = request.getHeader(name);
+			System.out.println(name+"===="+value);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
