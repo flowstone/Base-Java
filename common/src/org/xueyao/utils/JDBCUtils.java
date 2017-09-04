eacf89ddde677a0ab6c2b491b0d98b823776f845
flowstone <xueyao.me@gmail.com>
2017-09-04 11:09:45 +0800

    i found error

--- common/src/org/xueyao/utils/JDBCUtils.java
+++ common/src/org/xueyao/utils/JDBCUtils.java
@@ -35,35 +35,5 @@ public class JDBCUtils {
 		System.out.println(getConnection());
 	}
 	
-	
-	/**
-	 * 释放资源
-	 */
-	public static void release(Connection con,Statement st,ResultSet rs){
-		//6.释放资源。先开后关，后开先关。放在finally代码块中
-		if(rs!=null){
-			try {
-				rs.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		if(st!=null){
-			try {
-				st.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		if(con!=null){
-			try {
-				con.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+
 }
