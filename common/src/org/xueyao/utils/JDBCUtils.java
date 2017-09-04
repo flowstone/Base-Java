90f516beeef90eacdc86eb57a9224a30e1b66ae6
flowstone <xueyao.me@gmail.com>
2017-09-04 11:00:12 +0800

    模板项目-添加释放资源

--- common/src/org/xueyao/utils/JDBCUtils.java
+++ common/src/org/xueyao/utils/JDBCUtils.java
@@ -1,16 +1,19 @@
 package org.xueyao.utils;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.sql.DataSource;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class JDBCUtils {
-	//创建ComboPooledDataSource对象,读取的是命名的配置信息itcast
-	private static ComboPooledDataSource dataSource=new ComboPooledDataSource("itcast");
+	//创建ComboPooledDataSource对象,读取的是命名的配置信息xueyao
+	//private static ComboPooledDataSource dataSource=new ComboPooledDataSource("xueyao");
 	//创建ComboPooledDataSource对象,读取的是默认的配置信息
-	//private static ComboPooledDataSource dataSource=new ComboPooledDataSource();
+	private static ComboPooledDataSource dataSource=new ComboPooledDataSource();
 	
 	//获取连接
 	public static Connection getConnection(){
@@ -32,4 +35,35 @@ public class JDBCUtils {
 		System.out.println(getConnection());
 	}
 	
+	
+	/**
+	 * 释放资源
+	 */
+	public static void release(Connection con,Statement st,ResultSet rs){
+		//6.释放资源。先开后关，后开先关。放在finally代码块中
+		if(rs!=null){
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(st!=null){
+			try {
+				st.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(con!=null){
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
