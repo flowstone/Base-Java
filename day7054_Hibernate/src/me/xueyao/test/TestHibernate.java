f0bf9c58805ab771731978db40a9cfad8f707e89
flowstone <xueyao.me@gmail.com>
2017-09-27 16:29:40 +0800

    hibernate核心配置文件的第二种方式,不推荐使用

--- day7054_Hibernate/src/me/xueyao/test/TestHibernate.java
+++ day7054_Hibernate/src/me/xueyao/test/TestHibernate.java
@@ -39,5 +39,35 @@ public class TestHibernate {
 		session.close();
 		sf.close();
 	}
-
+	
+	@Test
+	public void test2() {
+		//要保存的对象 
+		Customer customer = new Customer();
+		customer.setCust_name("阿里");
+		customer.setCust_industry("卖假货的");
+		customer.setCust_level("VIP客户");
+		customer.setCust_source("小卡片");
+		customer.setCust_address("浙江杭州");
+		customer.setCust_phone("8650123");
+		
+		//实例化Configuration对象
+		Configuration cfg = new Configuration();
+		//添加映射文件
+		cfg.addResource("me/xueyao/domain/Customer.hbm.xml");
+		
+		//构造SessionFactory
+		SessionFactory sf = cfg.buildSessionFactory();
+		//获取Session,类似于JDBC时的Connection
+		Session session = sf.openSession();
+		//开启事务
+		session.beginTransaction();
+		//保存
+		session.save(customer);
+		//提交事务
+		session.getTransaction().commit();
+		//释放资源
+		session.close();
+		sf.close();
+	}
 }
