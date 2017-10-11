1dd0e8905621ccda0d8e2c980bf1a6754ab31b0f
flowstone <xueyao.me@gmail.com>
2017-10-11 09:30:21 +0800

    Struts2-拦截器的第一种写法

---
+++ day7061_Struts2_Interceptor/src/me/xueyao/interceptor/Test1Interceptor.java
@@ -0,0 +1,44 @@
+package me.xueyao.interceptor;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
+public class Test1Interceptor implements Interceptor{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 销毁方法
+     */
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    /**
+     * 初始化方法
+     */
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /**
+     * 拦截方法
+     * @param ActionInvocation    Action调试对象
+     * @return String 
+     */
+    @Override
+    public String intercept(ActionInvocation invocation) throws Exception {
+        System.out.println("请求经过测试拦截器1...");
+        //执行下一步,result其实是Action中方法的返回值
+        String result = invocation.invoke();
+        return result;
+    }
+
+}
