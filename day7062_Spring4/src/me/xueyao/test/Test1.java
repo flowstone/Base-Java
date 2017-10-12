7a79f14caa830c8711f437e66380c833aeaee448
flowstone <xueyao.me@gmail.com>
2017-10-12 16:00:35 +0800

    早期版本的Spring工厂:BeanFactory

--- day7062_Spring4/src/me/xueyao/test/Test1.java
+++ day7062_Spring4/src/me/xueyao/test/Test1.java
@@ -1,9 +1,12 @@
 package me.xueyao.test;
 
 import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 import me.xueyao.dao.UserDao;
 
@@ -20,6 +23,17 @@ public class Test1 {
     public void test2() {
         ApplicationContext ac = new FileSystemXmlApplicationContext("F:/Code/applicationContext.xml");
         UserDao userDao = (UserDao) ac.getBean("userDao");
+        userDao.save();
+    }
+    
+    /**
+     * 早期版本的Spring工厂：BeanFactory,现已弃用
+     */
+    @Test
+    public void test3() {
+        BeanFactory bf = new XmlBeanFactory(new ClassPathResource("applicationContext.xml"));
+        System.out.println("=====工厂对象创建出来了===========");
+        UserDao userDao = (UserDao) bf.getBean("userDao");
         userDao.save();
     }
 }
