24e61ab91e4641cfcb8ef383306f14fb2c8e2d93
flowstone <xueyao.me@gmail.com>
2017-10-12 15:48:45 +0800

    Spring-读取配置文件(磁盘方向)

--- day7062_Spring4/src/me/xueyao/test/Test1.java
+++ day7062_Spring4/src/me/xueyao/test/Test1.java
@@ -3,6 +3,7 @@ package me.xueyao.test;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import me.xueyao.dao.UserDao;
 
@@ -11,6 +12,13 @@ public class Test1 {
     @Test
     public void test1() {
         ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserDao userDao = (UserDao) ac.getBean("userDao");
+        userDao.save();
+    }
+    
+    @Test
+    public void test2() {
+        ApplicationContext ac = new FileSystemXmlApplicationContext("F:/Code/applicationContext.xml");
         UserDao userDao = (UserDao) ac.getBean("userDao");
         userDao.save();
     }
