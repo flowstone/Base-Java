5efbbf015648f79689db6413a61bb2e2917b8b3e
flowstone <xueyao.me@gmail.com>
2017-10-14 21:17:59 +0800

    Spring4-全注解-初代产品

--- day7063_Spring4_springAndDBUtilsFullAnnotation/src/me/xueyao/test/Test1.java
+++ day7063_Spring4_springAndDBUtilsFullAnnotation/src/me/xueyao/test/Test1.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import me.xueyao.config.SpringConfig;
 import me.xueyao.domain.Customer;
 import me.xueyao.service.CustomerService;
 
@@ -13,7 +14,8 @@ public class Test1 {
     
     @Test
     public void test1() {
-        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfig.class);
         CustomerService customerService = (CustomerService) ac.getBean("customerService");
         List<Customer> customers = customerService.findAllCustomer();
         for (Customer customer : customers) {
