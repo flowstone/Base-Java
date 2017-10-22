c467fb25422e50a45cc2fd2777109e75b9068fc5
flowstone <xueyao.me@gmail.com>
2017-10-23 02:02:37 +0800

    SSH(xml)-Spring+Hibernate整合优化及事务控制

--- day7066_SSH01/src/me/xueyao/ssh/dao/impl/CustomerDaoImpl.java
+++ day7066_SSH01/src/me/xueyao/ssh/dao/impl/CustomerDaoImpl.java
@@ -1,34 +1,37 @@
 package me.xueyao.ssh.dao.impl;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
 import me.xueyao.ssh.dao.CustomerDao;
 import me.xueyao.ssh.domain.Customer;
 
-public class CustomerDaoImpl implements CustomerDao {
-
+public class CustomerDaoImpl extends HibernateDaoSupport implements CustomerDao {
+/*
     private SessionFactory sessionFactory;
-    
     
     
     public void setSessionFactory(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
-
+*/
+    
+    
+    
     @Override
     public void save(Customer customer) {
         System.out.println("客户保存...");
-        Session session = sessionFactory.getCurrentSession();
+        /*Session session = sessionFactory.getCurrentSession();
         session.beginTransaction();
         session.save(customer);
-        session.getTransaction().commit();
+        session.getTransaction().commit();*/
+        this.getHibernateTemplate().save(customer);
     }
 
     @Override
     public Customer findById(Long id) {
         System.out.println("根据id查询客户...");
-        return null;
+        return this.getHibernateTemplate().get(Customer.class, id);
     }
 
 }
--- day7066_SSH01/src/me/xueyao/ssh/service/impl/CustomerServiceImpl.java
+++ day7066_SSH01/src/me/xueyao/ssh/service/impl/CustomerServiceImpl.java
@@ -8,12 +8,11 @@ public class CustomerServiceImpl implements CustomerService {
     
     private CustomerDao customerDao; //依赖注入
     
-    
-
     public void setCustomerDao(CustomerDao customerDao) {
         this.customerDao = customerDao;
     }
-
+    
+    
     @Override
     public void saveCustomer(Customer customer) {
         customerDao.save(customer);
