73765b5510dd48e40ce2d0418c8b6b3f90ffe773
flowstone <xueyao.me@gmail.com>
2017-10-23 01:16:46 +0800

    SSH-Hibernate单独运行

--- day7066_SSH01/src/me/xueyao/ssh/test/TestSSH.java
+++ day7066_SSH01/src/me/xueyao/ssh/test/TestSSH.java
@@ -1,11 +1,15 @@
 package me.xueyao.ssh.test;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import me.xueyao.ssh.domain.Customer;
 import me.xueyao.ssh.service.CustomerService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -14,10 +18,33 @@ public class TestSSH {
     
     @Autowired
     private CustomerService customerService;
-    
+    /**
+     * 测试Spring环境是否搭建好
+     */
     @Test
     public void test1() {
         customerService.saveCustomer(null);
         customerService.findCustomerById(1L);
+    }
+    
+    /**
+     * 测试hibernate环境是否搭建好
+     */
+    @Test
+    public void test2() {
+        Customer customer = new Customer();
+        customer.setCust_name("阿里");
+        
+        Configuration cfg = new Configuration();
+        //读取hibernate.cfg.xml
+        cfg.configure();
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.getCurrentSession();
+        session.beginTransaction();
+        session.save(customer);
+        session.getTransaction().commit();
+        
+        session.close();
+        sf.close();
     }
 }
