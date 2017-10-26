9401821a5b85206985f0360eaf3412ebdc3578d8
flowstone <xueyao.me@gmail.com>
2017-10-26 18:42:39 +0800

    struts2处理ajax请求注解的写法

--- day7068_Struts2_ajax/src/me/xueyao/action/AjaxAction.java
+++ day7068_Struts2_ajax/src/me/xueyao/action/AjaxAction.java
@@ -1,13 +1,19 @@
 package me.xueyao.action;
 
-import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
 
+import com.opensymphony.xwork2.ActionSupport;
+@ParentPackage("json-default")
+@Namespace("/")
 public class AjaxAction extends ActionSupport {
 
     private static final long serialVersionUID = 1L;
     
     private String result; //在对象栈里，因为json结果集要取这个值 
-    
+    @Action(value="ajaxAction",results={@Result(name="success",type="json",params={"root","result"})})
     public String ajax() {
         //调用业务，调用dao
         result = "这是ajax的结果";
