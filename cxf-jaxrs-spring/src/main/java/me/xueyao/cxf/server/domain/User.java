e90bf2a8cd261ab7032bb537db8c14c9d9f9f4c4
flowstone <xueyao.me@gmail.com>
2017-11-08 16:50:01 +0800

    RESTful的CXF与Spring整合

---
+++ cxf-jaxrs-spring/src/main/java/me/xueyao/cxf/server/domain/User.java
@@ -0,0 +1,56 @@
+package me.xueyao.cxf.server.domain;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author XueYao
+ * @date 2017-11-06
+ */
+@XmlRootElement(name = "user") //表述时根元素的名字，默认是类名第一个字母小写
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Integer id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
