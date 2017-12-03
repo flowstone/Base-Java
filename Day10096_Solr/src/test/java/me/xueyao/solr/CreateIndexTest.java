4b00e24f7ab4cbc3a85fcba65617faf4641a14ed
flowstone <xueyao.me@gmail.com>
2017-12-03 23:37:51 +0800

    Solr

---
+++ Day10096_Solr/src/test/java/me/xueyao/solr/CreateIndexTest.java
@@ -0,0 +1,14 @@
+package me.xueyao.solr;
+
+import org.junit.Test;
+
+/**
+ * @author XueYao
+ * @date 2017-12-03
+ */
+public class CreateIndexTest {
+    @Test
+    public void testCreateIndex() {
+
+    }
+}
