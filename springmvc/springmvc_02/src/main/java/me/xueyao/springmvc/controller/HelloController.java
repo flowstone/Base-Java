58474f9817a8be45302e9ea2addb718faae1426a
flowstone <xueyao.me@gmail.com>
2017-12-10 19:57:50 +0800

    SpringMVC-组合注解

--- springmvc/springmvc_02/src/main/java/me/xueyao/springmvc/controller/HelloController.java
+++ springmvc/springmvc_02/src/main/java/me/xueyao/springmvc/controller/HelloController.java
@@ -1,7 +1,7 @@
 package me.xueyao.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -16,6 +16,38 @@ public class HelloController {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("hello");
         mv.addObject("msg", "SpringMVC测试方法");
+        return mv;
+    }
+
+    @GetMapping(value = "show013")
+    public ModelAndView test013() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("hello");
+        mv.addObject("msg", "GetMapping");
+        return mv;
+    }
+
+    @PostMapping(value = "show014")
+    public ModelAndView test014() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("hello");
+        mv.addObject("msg", "PostMapping");
+        return mv;
+    }
+
+    @PutMapping(value = "show015")
+    public ModelAndView test015() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("hello");
+        mv.addObject("msg", "PutMapping");
+        return mv;
+    }
+
+    @DeleteMapping(value = "show016")
+    public ModelAndView test016() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("hello");
+        mv.addObject("msg", "DeleteMapping");
         return mv;
     }
 }
