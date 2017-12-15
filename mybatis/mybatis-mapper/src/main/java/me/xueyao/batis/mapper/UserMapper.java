2dafadcf03387562394f813d24408fe2a2d76c70
flowstone <xueyao.me@gmail.com>
2017-12-15 09:09:43 +0800

    Mybatis-通用Mapper

---
+++ mybatis/mybatis-mapper/src/main/java/me/xueyao/batis/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package me.xueyao.batis.mapper;
+
+import com.github.abel533.mapper.Mapper;
+import me.xueyao.batis.pojo.User;
+
+/**
+ * @author XueYao
+ * @date 2017-12-14
+ */
+public interface UserMapper extends Mapper<User>{
+}
